f764cff4c5c998e4f6cd1e9ce53a75303bd1fe41
Fernando Ticona <46249834+fvcoder@users.noreply.github.com>
2025-03-25 21:17:33 +0000

    data

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,12 @@
 import { glob } from "astro/loaders";
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const people = defineCollection({
   loader: glob({ pattern: "*.json", base: "src/content/people" }),
   schema: z.object({
     name: z.string(),
     bio: z.string(),
+    image: z.string(),
     networks: z.array(
       z.object({
         provider: z.string(),
@@ -22,6 +23,7 @@ const event = defineCollection({
     name: z.string(),
     description: z.string(),
     image: z.string(),
+    location: reference("location"),
     sponsors: z.array(
       z.object({
         name: z.string(),
@@ -37,4 +39,34 @@ const event = defineCollection({
   }),
 });
 
-export const collections = { people, event };
+const schedule = defineCollection({
+  loader: glob({ pattern: "*.json", base: "src/content/schedule" }),
+  schema: z.object({
+    name: z.string(),
+    description: z.string(),
+    stageId: reference("stage"),
+    eventId: reference("event"),
+    speakers: z.array(reference("people")),
+    startDate: z.string(),
+    endDate: z.string().optional(),
+  }),
+});
+
+const location = defineCollection({
+  loader: glob({ pattern: "*.json", base: "src/content/location" }),
+  schema: z.object({
+    name: z.string(),
+    direction: z.string(),
+    image: z.string(),
+    url: z.string().url().optional(),
+  }),
+});
+
+const stage = defineCollection({
+  loader: glob({ pattern: "*.json", base: "src/content/stage" }),
+  schema: z.object({
+    name: z.string(),
+  }),
+});
+
+export const collections = { people, event, schedule, location, stage };
